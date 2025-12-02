fbcc30d86ce44b8e7d385d364b5860fcc4d062ed
tanmay211106-beep <tanmay211106@gmail.com>
2025-12-03 02:33:13 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
-    domains: [], // add any external image host domains you use
-    formats: ['image/avif', 'image/webp'],
+    domains: [], // add image domains if needed
+    formats: ["image/avif", "image/webp"],
   },
-  experimental: {
-    appDir: true,
-  }
 };
 
 module.exports = nextConfig;
